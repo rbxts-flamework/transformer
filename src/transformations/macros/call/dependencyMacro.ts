1b03652d796456df319011fefe7e72729261c89a
Firebolt <24422634+Fireboltofdeath@users.noreply.github.com>
2021-06-14 15:35:46 -0400

    Flatten dependency macro and add diagnostics

--- src/transformations/macros/call/dependencyMacro.ts
+++ src/transformations/macros/call/dependencyMacro.ts
@@ -1,4 +1,4 @@
-import ts from "typescript";
+import { Diagnostics } from "../../../classes/diagnostics";
 import { f } from "../../../util/factory";
 import { CallMacro } from "../macro";
 
@@ -9,32 +9,37 @@ export const DependencyMacro: CallMacro = {
 
 	transform(state, node) {
 		const importId = state.addFileImport(state.getSourceFile(node), "@rbxts/flamework", "Flamework");
-		const argument = node.arguments[0];
-		if (argument && ts.isIdentifier(argument)) {
-			const symbol = state.getSymbol(argument);
-			if (!symbol) return f.nil();
+		const firstArg = node.arguments[0];
+		const firstType = node.typeArguments?.[0];
+
+		if (firstArg && !firstType) {
+			if (!f.is.identifier(firstArg)) Diagnostics.error(firstArg, `Expected identifier`);
+
+			const symbol = state.getSymbol(firstArg);
+			if (!symbol) Diagnostics.error(firstArg, `Could not find symbol`);
 
 			const declaration = symbol.declarations?.[0];
-			if (!declaration) return f.nil();
+			if (!declaration) Diagnostics.error(firstArg, `Could not find declaration`);
 
 			return f.as(
 				f.call(f.field(importId, "resolveDependency"), [state.getUid(declaration)]),
-				f.referenceType(argument),
+				f.referenceType(firstArg),
 			);
-		} else {
-			const firstType = node.typeArguments?.[0];
-			if (!f.is.referenceType(firstType)) return f.nil();
+		} else if (firstType && !firstArg) {
+			if (!f.is.referenceType(firstType)) Diagnostics.error(node, `Expected type reference`);
 
 			const symbol = state.getSymbol(firstType.typeName);
-			if (!symbol) return f.nil();
+			if (!symbol) Diagnostics.error(firstType, `Could not find symbol`);
 
 			const declaration = symbol.declarations?.[0];
-			if (!declaration) return f.nil();
+			if (!declaration) Diagnostics.error(firstType, `Could not find declaration`);
 
 			return f.as(
 				f.call(f.field(importId, "resolveDependency"), [f.string(state.getUid(declaration))]),
 				firstType,
 			);
+		} else {
+			Diagnostics.error(node, `Could not find specifier`);
 		}
 	},
 };
