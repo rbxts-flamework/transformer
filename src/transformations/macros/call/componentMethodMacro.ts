7f47567ac404f1f33fc27cfed71d7d99e5f1d394
Firebolt <24422634+Fireboltofdeath@users.noreply.github.com>
2021-06-14 14:57:08 -0400

    Flatten components macros and add diagnostics

--- src/transformations/macros/call/componentMethodMacro.ts
+++ src/transformations/macros/call/componentMethodMacro.ts
@@ -1,4 +1,5 @@
 import ts from "typescript";
+import { Diagnostics } from "../../../classes/diagnostics";
 import { f } from "../../../util/factory";
 import { CallMacro } from "../macro";
 
@@ -14,27 +15,30 @@ export const ComponentMethodMacro: CallMacro = {
 
 	transform(state, node) {
 		const firstType = node.typeArguments?.[0];
-		if (f.is.referenceType(firstType)) {
+		if (firstType) {
+			if (!f.is.referenceType(firstType)) Diagnostics.error(firstType, `Expected type reference`);
+
 			const declaration = state.getSymbol(firstType.typeName)?.declarations?.[0];
-			if (declaration) {
-				return f.update.call(node, undefined, [
-					node.arguments[0],
-					f.as(f.string(state.getUid(declaration)), f.keywordType(ts.SyntaxKind.NeverKeyword)),
-				]);
-			}
+			if (!declaration) Diagnostics.error(firstType, `Could not find declaration`);
+
+			return f.update.call(node, undefined, [
+				node.arguments[0],
+				f.as(f.string(state.getUid(declaration)), f.keywordType(ts.SyntaxKind.NeverKeyword)),
+			]);
 		} else {
 			const specifier = node.arguments[1];
-			if (specifier) {
-				const symbol = state.getSymbol(specifier);
-				const declaration = symbol?.declarations?.[0];
-				if (symbol && state.classes.has(symbol) && declaration) {
-					return f.update.call(node, undefined, [
-						node.arguments[0],
-						f.as(f.string(state.getUid(declaration)), f.keywordType(ts.SyntaxKind.NeverKeyword)),
-					]);
-				}
-			}
+			if (!specifier) Diagnostics.error(node, `No specifier found`);
+
+			const symbol = state.getSymbol(specifier);
+			if (!symbol) Diagnostics.error(specifier, `Symbol could not be found`);
+
+			const declaration = symbol?.declarations?.[0];
+			if (!declaration) Diagnostics.error(specifier, `Declaration could not be found`);
+
+			return f.update.call(node, undefined, [
+				node.arguments[0],
+				f.as(f.string(state.getUid(declaration)), f.keywordType(ts.SyntaxKind.NeverKeyword)),
+			]);
 		}
-		return node;
 	},
 };
