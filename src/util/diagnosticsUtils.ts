8b70c052b22dba9b96e6524987965ccae597eabe
Firebolt <24422634+Fireboltofdeath@users.noreply.github.com>
2021-07-03 22:24:54 -0400

    Enable diagnostics for dev builds

--- src/util/diagnosticsUtils.ts
+++ src/util/diagnosticsUtils.ts
@@ -11,7 +11,11 @@ export function captureDiagnostic<T, A extends unknown[]>(cb: (...args: A) => T,
 	} catch (e) {
 		if ("diagnostic" in e) {
 			/// Temporary workaround for 1.1.1
-			if (ts.version.startsWith("1.1.1") && !(globalThis as { RBXTSC_DEV?: boolean }).RBXTSC_DEV) {
+			if (
+				ts.version.startsWith("1.1.1") &&
+				!ts.version.startsWith("1.1.1-dev") &&
+				!(globalThis as { RBXTSC_DEV?: boolean }).RBXTSC_DEV
+			) {
 				e.diagnostic = undefined;
 				throw e;
 			}
