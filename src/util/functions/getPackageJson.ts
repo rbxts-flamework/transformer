af0a7a263212f38b3ab6aa933cc3c24b7a09033a
Firebolt <24422634+Fireboltofdeath@users.noreply.github.com>
2022-02-24 04:36:46 -0500

    Add caching to getPackageJson

--- src/util/functions/getPackageJson.ts
+++ src/util/functions/getPackageJson.ts
@@ -1,12 +1,34 @@
 import ts from "typescript";
 import path from "path";
 import normalize from "normalize-package-data";
+import { isPathDescendantOf } from "./isPathDescendantOf";
+import { Cache } from "../cache";
+
+export type PackageJsonResult = ReturnType<typeof getPackageJsonInner>;
 
 /**
  * Looks recursively at ancestors until a package.json is found
  * @param directory The directory to start under.
  */
 export function getPackageJson(directory: string) {
+	const existing = Cache.pkgJsonCache.get(path.normalize(directory));
+	if (existing) return existing;
+
+	const result = getPackageJsonInner(directory);
+
+	Cache.pkgJsonCache.set(path.normalize(directory), result);
+	ts.forEachAncestorDirectory(directory, (dir) => {
+		if (isPathDescendantOf(dir, result.directory)) {
+			Cache.pkgJsonCache.set(path.normalize(dir), result);
+		} else {
+			return true;
+		}
+	});
+
+	return result;
+}
+
+function getPackageJsonInner(directory: string) {
 	const packageJsonPath = ts.findPackageJson(directory, ts.sys as never);
 	if (!packageJsonPath) throw new Error(`package.json not found in ${directory}`);
 
