7c5a0e6128ddfef2e6efd35f40eb9d6668cb8184
fireboltofdeath <24422634+Fireboltofdeath@users.noreply.github.com>
2022-04-22 21:39:17 -0400

    Remove component constructor transformations

--- src/transformations/statements/transformClassDeclaration.ts
+++ src/transformations/statements/transformClassDeclaration.ts
@@ -3,15 +3,9 @@ import ts from "typescript";
 import { Diagnostics } from "../../classes/diagnostics";
 import { NodeMetadata } from "../../classes/nodeMetadata";
 import { TransformState } from "../../classes/transformState";
-import { DecoratorWithNodes } from "../../types/decorators";
 import { f } from "../../util/factory";
-import { addLeadingComment } from "../../util/functions/addLeadingComment";
 import { buildGuardFromType, buildGuardsFromType } from "../../util/functions/buildGuardFromType";
-import { getInferExpression } from "../../util/functions/getInferExpression";
-import { getPrettyName } from "../../util/functions/getPrettyName";
 import { getSuperClasses } from "../../util/functions/getSuperClasses";
-import { getUniversalTypeNodeGenerator } from "../../util/functions/getUniversalTypeNode";
-import { replaceValue } from "../../util/functions/replaceValue";
 import { getNodeUid, getSymbolUid, getTypeUid } from "../../util/uid";
 
 export function transformClassDeclaration(state: TransformState, node: ts.ClassDeclaration) {
@@ -41,27 +35,11 @@ export function transformClassDeclaration(state: TransformState, node: ts.ClassD
 			}
 		}
 
-		if (classInfo.decorators.some((x) => x.isFlameworkDecorator && x.name === "Component")) {
-			const onStartId = getSymbolUid(state, state.symbolProvider.flameworkFile.get("OnStart"));
-			assert(onStartId);
-			if (!implementClauses.some((x) => x.text === onStartId)) {
-				const existingOnStart = node.members.find((x) =>
-					x.name && "text" in x.name ? x.name.text === "onStart" : false,
-				);
-				if (existingOnStart !== undefined) {
-					Diagnostics.error(existingOnStart, "Components can not have a member named 'onStart'");
-				}
-
-				implementClauses.push(f.string(onStartId));
-			}
-		}
-
 		if (implementClauses.length > 0 && metadata.isRequested("flamework:implements")) {
 			fields.push(["flamework:implements", f.array(implementClauses, false)]);
 		}
 	}
 
-	const decorators = classInfo.decorators.filter((x): x is DecoratorWithNodes => x.type === "WithNodes");
 	const importIdentifier = state.addFileImport(state.getSourceFile(node), "@flamework/core", "Reflect");
 	const realFields: ts.Statement[] = fields.map(([name, value]) =>
 		f.statement(f.call(f.field(importIdentifier, "defineMetadata"), [node.name!, name, value])),
@@ -80,7 +58,7 @@ export function transformClassDeclaration(state: TransformState, node: ts.ClassD
 		`(Flamework) ${node.name.text} metadata`,
 	);
 
-	return [updateClass(state, node, decorators), ...realFields];
+	return [updateClass(state, node), ...realFields];
 }
 
 function generateFieldMetadata(state: TransformState, metadata: NodeMetadata, field: ts.PropertyDeclaration) {
@@ -310,244 +288,32 @@ function getAssignabilityDiagnostics(
 	return diagnostic;
 }
 
-function updateClass(state: TransformState, node: ts.ClassDeclaration, decorators: DecoratorWithNodes[]) {
-	let members: ts.NodeArray<ts.ClassElement> | ts.ClassElement[] = node.members;
-
-	if (decorators.some((x) => x.isFlameworkDecorator && x.name === "Component")) {
-		let onStartIndex = members.findIndex((x) => x.name && "text" in x.name && x.name.text === "onStart");
-		let onStart = members[onStartIndex];
-
-		if (!onStart) {
-			onStartIndex = 0;
-			onStart = f.methodDeclaration("onStart", f.block([]));
-			members = [onStart, ...members];
-		}
-
-		if (f.is.methodDeclaration(onStart) && onStart.body) {
-			const propertyDeclarations = new Array<[ts.PropertyName, ts.Expression, boolean]>();
-
-			members = members.map((member) => {
-				if (!f.is.propertyDeclaration(member)) return state.transformNode(member);
-				if (member.modifierFlagsCache & ts.ModifierFlags.Static) return state.transformNode(member);
-				if (member.modifierFlagsCache & ts.ModifierFlags.Abstract) return state.transformNode(member);
-
-				if (member.initializer) {
-					propertyDeclarations.push([
-						member.name,
-						state.transformNode(member.initializer),
-						(member.modifierFlagsCache & ts.ModifierFlags.Readonly) !== 0,
-					]);
-				}
-
-				if (member.type) {
-					return f.update.propertyDeclaration(
-						member,
-						null,
-						undefined,
-						undefined,
-						undefined,
-						member.questionToken ? "?" : "!",
-						member.type,
-					);
-				}
-
-				const type = state.typeChecker.getTypeAtLocation(member.name);
-				const generator = getUniversalTypeNodeGenerator(member);
-				const validTypeNode = generator.generate(type);
-				if (validTypeNode) {
-					state.prereqList(generator.prereqs);
-					return f.update.propertyDeclaration(
-						member,
-						null,
-						undefined,
-						undefined,
-						undefined,
-						"!",
-						validTypeNode,
-					);
-				}
-
-				if (member.initializer) {
-					// HACK: if the type can't be represented as a TypeNode,
-					// use a generic function that returns nil to infer the type
-					const inferExpression = getInferExpression(state, state.getSourceFile(node));
-					return f.update.propertyDeclaration(
-						member,
-						f.call(inferExpression, [f.arrowFunction(member.initializer)]),
-					);
-				}
-
-				return state.transformNode(member);
-			});
-
-			const constructorStatements = new Array<ts.Statement>();
-			const constructorIndex = members.findIndex((x) => f.is.constructor(x));
-			const constructor = members[constructorIndex] as ts.ConstructorDeclaration;
-			if (constructor) {
-				const internalProp = f.identifier("constructor_parameters", true);
-				if (constructor.parameters.length) {
-					members.unshift(
-						f.propertyDeclaration(
-							internalProp,
-							undefined,
-							f.tupleType(constructor.parameters.map((x) => x.type!)),
-						),
-					);
-				}
-
-				const parameterNames = new Array<ts.Identifier>();
-				const parameters = constructor.parameters.map((parameter) => {
-					if (f.is.identifier(parameter.name)) {
-						parameterNames.push(parameter.name);
-						return parameter;
-					} else {
-						const tempId = f.identifier(getPrettyName(state, parameter.type, "binding"), true);
-						parameterNames.push(tempId);
-						constructorStatements.push(f.variableStatement(parameter.name, tempId));
-						return f.update.parameterDeclaration(parameter, tempId);
-					}
-				});
-
-				if (constructor.parameters.length) {
-					constructorStatements.unshift(
-						f.variableStatement(
-							f.arrayBindingDeclaration(parameterNames),
-							f.field(ts.factory.createThis(), internalProp),
-						),
-					);
-				}
-
-				const superCalls = new Array<ts.CallExpression>();
-				ts.forEachChildRecursively(constructor, (node, parent) => {
-					if (f.is.call(parent) && node.kind === ts.SyntaxKind.SuperKeyword) {
-						superCalls.push(parent);
-					}
-				});
-
-				if (superCalls.length > 1) Diagnostics.error(superCalls[1], "Expected one super() call in component");
-				const superCall = state.transformNode(superCalls[0]);
-
-				const setConstructorParameters = constructor.parameters.length
-					? f.statement(
-							f.binary(
-								f.field(ts.factory.createThis(), internalProp),
-								ts.SyntaxKind.EqualsToken,
-								parameterNames,
-							),
-					  )
-					: f.statement();
-
-				constructorStatements.push(...constructor.body!.statements.filter((x) => x !== superCall?.parent));
-
-				replaceValue(
-					members,
-					constructor,
-					f.update.constructor(
-						constructor,
-						parameters,
-						f.block(
-							superCall ? [f.statement(superCall), setConstructorParameters] : [setConstructorParameters],
-						),
-					),
-				);
-			}
-
-			const hasOnStart = shouldAddSuperOnStart(state, node);
-			const overrideModifier =
-				hasOnStart && !ts.findModifier(onStart, ts.SyntaxKind.OverrideKeyword)
-					? [ts.factory.createModifier(ts.SyntaxKind.OverrideKeyword)]
-					: [];
-
-			const superOnStartStatement = hasOnStart
-				? f.statement(f.call(f.field(f.superExpression(), "onStart")))
-				: f.statement();
-
-			const superOnStart = ts.forEachChildRecursively(onStart, (node) => {
-				if (!f.is.call(node)) return;
-
-				const expr = node.expression;
-				if (!f.is.accessExpression(expr)) return;
-				if (!f.is.superExpression(expr.expression)) return;
-
-				if (f.is.propertyAccessExpression(expr)) {
-					if (expr.name.text === "onStart") {
-						return node;
-					}
-				} else if (f.is.elementAccessExpression(expr)) {
-					if (f.is.string(expr.argumentExpression)) {
-						return expr.argumentExpression.text === "onStart" ? expr : undefined;
-					}
-				}
-			});
-
-			if (superOnStart) {
-				Diagnostics.error(superOnStart, "super.onStart() must be omitted.");
-			}
-
-			const transformedProperties = propertyDeclarations.map(([name, initializer, isReadonly]) => {
-				const readonlyThis =
-					isReadonly &&
-					f.as(
-						f.self(),
-						f.typeLiteralType([f.propertySignatureType(name, f.keywordType(ts.SyntaxKind.UnknownKeyword))]),
-						true,
-					);
-
-				return f.statement(
-					f.binary(f.field(readonlyThis || f.self(), name), f.token(ts.SyntaxKind.EqualsToken), initializer),
-				);
-			});
-
-			const constructorBody = constructorStatements.length
-				? addLeadingComment(f.block(sanitizeConstructorBody(state, constructorStatements)), "Constructor Body")
-				: f.statement();
-
-			addLeadingComment(transformedProperties[0], "Property Declarations");
-			addLeadingComment(onStart.body.statements[0], "OnStart Event");
-
-			onStartIndex = members.findIndex((x) => x.name && "text" in x.name && x.name.text === "onStart");
-			members[onStartIndex] = f.update.methodDeclaration(
-				onStart,
-				undefined,
-				f.block([
-					superOnStartStatement,
-					...transformedProperties,
-					constructorBody,
-					...state.transformList(onStart.body.statements),
-				]),
-				undefined,
-				undefined,
-				undefined,
-				onStart.modifiers ? [...onStart.modifiers, ...overrideModifier] : overrideModifier,
-			);
-		}
-	} else {
-		members = members.map((node) => state.transformNode(node));
-	}
-
+function updateClass(state: TransformState, node: ts.ClassDeclaration) {
 	return f.update.classDeclaration(
 		node,
 		node.name ? state.transformNode(node.name) : undefined,
-		members.map((member) => {
-			// Strip Flamework decorators from members
-			if (member.decorators) {
-				const filteredDecorators = transformDecorators(state, member.decorators);
-				if (f.is.propertyDeclaration(member)) {
-					return f.update.propertyDeclaration(member, undefined, undefined, filteredDecorators);
-				} else if (f.is.methodDeclaration(member)) {
-					return f.update.methodDeclaration(
-						member,
-						undefined,
-						undefined,
-						undefined,
-						undefined,
-						filteredDecorators,
-					);
+		node.members
+			.map((node) => state.transformNode(node))
+			.map((member) => {
+				// Strip Flamework decorators from members
+				if (member.decorators) {
+					const filteredDecorators = transformDecorators(state, member.decorators);
+					if (f.is.propertyDeclaration(member)) {
+						return f.update.propertyDeclaration(member, undefined, undefined, filteredDecorators);
+					} else if (f.is.methodDeclaration(member)) {
+						return f.update.methodDeclaration(
+							member,
+							undefined,
+							undefined,
+							undefined,
+							undefined,
+							filteredDecorators,
+						);
+					}
 				}
-			}
 
-			return member;
-		}),
+				return member;
+			}),
 		node.decorators && transformDecorators(state, node.decorators),
 	);
 }
@@ -559,43 +325,6 @@ function transformDecorators(state: TransformState, decorators: readonly ts.Deco
 			return type.getProperty("_flamework_Decorator") === undefined;
 		})
 		.map((decorator) => state.transform(decorator));
-}
-
-function sanitizeConstructorBody(state: TransformState, statements: ts.Statement[]) {
-	const visitor = (node: ts.Node): ts.Node => {
-		if (f.is.accessExpression(node)) {
-			const name = ts.getNameOfAccessExpression(node);
-			const symbol = state.getSymbol(name);
-			if (
-				symbol &&
-				symbol.valueDeclaration &&
-				symbol.valueDeclaration.modifierFlagsCache & ts.ModifierFlags.Readonly &&
-				f.is.propertyDeclaration(symbol.valueDeclaration)
-			) {
-				const sanitizedThis = f.as(f.self(), f.referenceType("Writable", [f.selfType()]), true);
-				return f.is.elementAccessExpression(node)
-					? f.elementAccessExpression(sanitizedThis, name as ts.Expression)
-					: f.propertyAccessExpression(sanitizedThis, name as ts.MemberName);
-			}
-		}
-		return ts.visitEachChild(node, visitor, state.context);
-	};
-	return statements.map((node) => ts.visitNode(node, visitor));
-}
-
-function shouldAddSuperOnStart(state: TransformState, node: ts.ClassDeclaration) {
-	const superClass = getSuperClasses(state.typeChecker, node)[0];
-	if (!superClass) return false;
-
-	const symbol = state.getSymbol(superClass);
-	if (!symbol) Diagnostics.error(superClass.name ?? node.name ?? node, "Could not find symbol");
-
-	if (symbol.members?.has("onStart" as ts.__String)) return true;
-
-	const classInfo = state.classes.get(symbol);
-	if (!classInfo) return false;
-
-	return classInfo.decorators.some((x) => x.isFlameworkDecorator && x.name === "Component");
 }
 
 function calculateOmittedGuards(
