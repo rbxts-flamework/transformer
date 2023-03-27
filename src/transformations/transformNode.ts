93c7e403d7902e4331c4ab4b58104f6b15c4c517
fireboltofdeath <24422634+Fireboltofdeath@users.noreply.github.com>
2023-03-27 12:01:48 -0400

    Add fail-safe for errors when transforming the AST

--- src/transformations/transformNode.ts
+++ src/transformations/transformNode.ts
@@ -1,13 +1,22 @@
 import ts from "typescript";
+import { Diagnostics } from "../classes/diagnostics";
 import { TransformState } from "../classes/transformState";
 import { transformExpression } from "./transformExpression";
 import { transformStatement } from "./transformStatement";
 
 export function transformNode(state: TransformState, node: ts.Node): ts.Node | ts.Statement[] {
-	if (ts.isExpression(node)) {
-		return transformExpression(state, node);
-	} else if (ts.isStatement(node)) {
-		return transformStatement(state, node);
+	try {
+		if (ts.isExpression(node)) {
+			return transformExpression(state, node);
+		} else if (ts.isStatement(node)) {
+			return transformStatement(state, node);
+		}
+	} catch (e) {
+		if (e instanceof Error && !("diagnostic" in e)) {
+			Diagnostics.error(node, `Flamework failure occured here\n${e.stack}`);
+		}
+
+		throw e;
 	}
 
 	return ts.visitEachChild(node, (newNode) => transformNode(state, newNode), state.context);
