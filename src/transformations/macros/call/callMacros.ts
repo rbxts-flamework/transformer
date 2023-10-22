d5070cb4f83bae188cc39d7ecc44f8a80ef3b244
fireboltofdeath <24422634+Fireboltofdeath@users.noreply.github.com>
2023-10-22 14:36:28 -0400

    Remove connect networking macro

--- src/transformations/macros/call/callMacros.ts
+++ src/transformations/macros/call/callMacros.ts
@@ -4,13 +4,9 @@ import { FlameworkCreateGuardMacro } from "./core/flamework/createGuardMacro";
 import { FlameworkHashMacro } from "./core/flamework/hashMacro";
 import { FlameworkIdMacro } from "./core/flamework/flameworkIdMacro";
 import { FlameworkImplementsMacro } from "./core/flamework/implementsMacro";
-import { NetworkingConnectMacro } from "./networking/connectMacro";
 import { GenericIdMacro } from "./core/genericIdMacro";
 
 export const CALL_MACROS = new Array<CallMacro>(
-	// @flamework/networking
-	NetworkingConnectMacro,
-
 	// @flamework/core
 	GenericIdMacro,
 	DependencyMacro,
