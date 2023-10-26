6db4e060d929615ff8667b54ab22531d436ffdec
fireboltofdeath <24422634+Fireboltofdeath@users.noreply.github.com>
2023-10-25 22:13:00 -0400

    Remove id macro

--- src/transformations/macros/call/callMacros.ts
+++ src/transformations/macros/call/callMacros.ts
@@ -1,11 +1,9 @@
 import { CallMacro } from "../macro";
 import { DependencyMacro } from "./core/dependencyMacro";
-import { FlameworkIdMacro } from "./core/flamework/flameworkIdMacro";
 import { GenericIdMacro } from "./core/genericIdMacro";
 
 export const CALL_MACROS = new Array<CallMacro>(
 	// @flamework/core
 	GenericIdMacro,
 	DependencyMacro,
-	FlameworkIdMacro,
 );
