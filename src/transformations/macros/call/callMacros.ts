17a888feb96fc5369057de87931452933be724f4
fireboltofdeath <24422634+Fireboltofdeath@users.noreply.github.com>
2023-10-25 22:20:46 -0400

    Remove Dependency macro

--- src/transformations/macros/call/callMacros.ts
+++ src/transformations/macros/call/callMacros.ts
@@ -1,9 +1,7 @@
 import { CallMacro } from "../macro";
-import { DependencyMacro } from "./core/dependencyMacro";
 import { GenericIdMacro } from "./core/genericIdMacro";
 
 export const CALL_MACROS = new Array<CallMacro>(
 	// @flamework/core
 	GenericIdMacro,
-	DependencyMacro,
 );
