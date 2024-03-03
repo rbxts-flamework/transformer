2e01ddc35befe87f8c019b1ecea3ba99078a735e
fireboltofdeath <24422634+Fireboltofdeath@users.noreply.github.com>
2024-03-02 19:16:44 -0500

    Fix compiler diagnostics when extending a user macro class

--- src/transformations/expressions/transformCallExpression.ts
+++ src/transformations/expressions/transformCallExpression.ts
@@ -2,12 +2,18 @@ import ts from "typescript";
 import { TransformState } from "../../classes/transformState";
 import { transformNode } from "../transformNode";
 import { transformUserMacro } from "../transformUserMacro";
+import { f } from "../../util/factory";
 
 export function transformCallExpression(state: TransformState, node: ts.CallExpression) {
 	const symbol = state.getSymbol(node.expression);
 
 	if (symbol) {
 		if (state.isUserMacro(symbol)) {
+			// We skip `super()` expressions as we likely do not have enough information to evaluate it.
+			if (f.is.superExpression(node.expression)) {
+				return state.transform(node);
+			}
+
 			const signature = state.typeChecker.getResolvedSignature(node);
 			if (signature) {
 				return transformUserMacro(state, node, signature) ?? state.transform(node);
